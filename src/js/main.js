e12becd632623332356ca962ab3d94840709238f
codediaz <sdiazf99@outlook.com>
2022-07-29 18:47:24 -0500

    feature: functions for geometric shapes - Basic functions have been created

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,38 @@
 /*Main*/
+
+//Square
+function areaOfSquare(side){
+    return side**2;
+};
+
+function perimeterOfSquare(side){
+    return side*4;
+}
+
+//Triangle
+function areaOfTriangle(base,height){
+    return (base * height)/2;
+}
+
+function perimeterOfTriangle(side1,side2,base){
+    return side1+side2+base;
+}
+
+//Triangle isosceles
+function heightOfTriangleIsosceles(side, base){
+    const arguments = side**2 - (base**2/4);
+    return Math.sqrt(arguments);
+}
+
+//Circle 
+const PI = Math.PI;
+function diameterOfCircle(radio){
+    return radio*2;
+}
+function perimeterOfCircle(radio){
+    const diameter = diameterOfCircle(radio);
+    return diameter*PI
+}
+function areaOfCircle(radio){
+    return (radio**2)*PI
+}
